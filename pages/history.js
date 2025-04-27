d563a5271f1f11667eac956b5fb9e43f2bb0f0ec
A. R. Saravia <andresromansaravia@gmail.com>
2025-04-27 13:49:32 -0300

    history update

---
+++ pages/history.js
@@ -0,0 +1,61 @@
+// Inicialización de productos
+let products = [
+	{
+		"id": 1,
+		"title": "Producto 1",
+		"description": "Descripción 1",
+		"price": 20,
+		"stock": 5,
+	},
+	{
+		"id": 2,
+		"title": "Producto 2",
+		"description": "Descripción 2",
+		"price": 1,
+		"stock": 1000,
+	},
+	{
+		"id": 3,
+		"title": "Producto 3",
+		"description": "Descripción 3",
+		"price": 5,
+		"stock": 4,
+	}
+]
+if (localStorage.getItem("products") == null) {
+	localStorage.setItem("products", JSON.stringify(products))
+} else {
+	products = JSON.parse(localStorage.getItem("products"))
+}
+
+// Inicialización de historial de compra
+let purchaseHistory = []
+if (localStorage.getItem("purchaseHistory") == null) {
+	localStorage.setItem("purchaseHistory", JSON.stringify(purchaseHistory))
+} else {
+	purchaseHistory = JSON.parse(localStorage.getItem("purchaseHistory"))
+}
+
+let purchaseContainer = document.getElementById("purchase-list")
+function renderLogs(purchaseHistory){
+	products.forEach(product => {
+		const filteredProducts = purchaseHistory.filter((p) => p.product.id == product.id)
+		const quantitySum = filteredProducts.reduce((accumulator, currentValue) => accumulator + currentValue.quantity,0)
+		const totalSum = filteredProducts.reduce((accumulator, currentValue) => accumulator + currentValue.quantity*currentValue.product.price,0)
+		const allDates = filteredProducts.map((p) => Date.parse(p.date))
+		if (quantitySum!=0){
+			const purchaseItem = document.createElement("div")
+			purchaseItem.innerHTML = `<h3>Producto: ${product.id}</h3>
+								<p>Información del producto: ${JSON.stringify(product)}</p>
+								<p>Cantidad comprada: ${quantitySum}</p>
+								<p>Dinero invertido: ${totalSum}</p>
+								<p>Última compra: ${new Date(Math.max(...allDates))}</p>`
+			purchaseContainer.appendChild(purchaseItem)
+		}
+	})
+}
+renderLogs(purchaseHistory)
+
+purchaseHistory.forEach(purchase => {
+	console.log(purchase.product.id)
+})
