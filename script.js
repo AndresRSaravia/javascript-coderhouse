9570f9c171ec9acd2001bdca4f97a89cbf425af3
A. R. Saravia <andresromansaravia@gmail.com>
2025-04-27 13:49:23 -0300

    index update

--- script.js
+++ script.js
@@ -1,69 +1,117 @@
-let funds = 0;
+// Inicialización de productos
 let products = [
 	{
+		"id": 1,
 		"title": "Producto 1",
 		"description": "Descripción 1",
 		"price": 20,
 		"stock": 5,
 	},
 	{
+		"id": 2,
 		"title": "Producto 2",
 		"description": "Descripción 2",
 		"price": 1,
 		"stock": 1000,
+	},
+	{
+		"id": 3,
+		"title": "Producto 3",
+		"description": "Descripción 3",
+		"price": 5,
+		"stock": 4,
 	}
 ]
-
-isNotValidPurchase = (quantity,stock,funds,price) => {return !(Number(quantity)>0) || stock < quantity || funds < price*quantity}
-isNotValidFund = (inputfund) => {return !(Number(inputfund)>0)}
-productExplained = (product) => `Nombre: ${product.title}, descripción: ${product.description}, stock: ${product.stock}, precio: ${product.price}\n`
-
-let option = undefined;
-while(Number(option) !== 0){
-	option = prompt("Bienvenido a la tienda. Para cargar fondos, presione 1. Para comprar productos, presione 2. Para salir del menú, presione 0. Escriba su opción.");
-	console.log(`Opción elegida: ${option}.`)
-	switch (Number(option)) {
-		case 0:
-			alert("¡Gracias, vuelva prontos!");
-			break;
-		case 1:
-			let inputfund = prompt("Ingrese la cantidad a agregar.");
-			if (isNotValidFund(inputfund)) {
-				alert("Cantidad inválida.");
-			} else {
-				funds += Number(inputfund);
-				alert("Cantidad válida.");
-			}
-			break;
-		case 2:
-			let txt = ""
-			for(const product of products) {
-				txt += productExplained(product);
-			}
-			const title = prompt("Ingrese el nombre del producto de los siguientes:\n" + txt);
-			let productindex = -1;
-			for (const index of products.keys()) {
-				if (products[index].title === title) {
-					productindex = index;
-				}
-			}
-			if (productindex === -1) {
-				alert("Producto no encontrado.");
-			} else {
-				const stock = products[productindex].stock;
-				const price = products[productindex].price;
-				let quantity = prompt(`Ingrese la cantidad a comprar.\nstock: ${stock}, precio: ${price}, fondos: ${funds}`);
-				if (isNotValidPurchase(quantity,stock,funds,price)) {
-					alert("Error: La cantidad es inválida, supera el stock o no tienes suficientes fondos.");
-				} else {
-					products[productindex].stock -= quantity;
-					funds -= price*quantity;
-					alert("Compra realizada.");
-				}
-			}
-			break;
-		default:
-			alert("Ingrese un número válido: 0, 1, 2.");
-			break;
-	}
+if (localStorage.getItem("products") == null) {
+	localStorage.setItem("products", JSON.stringify(products))
+} else {
+	products = JSON.parse(localStorage.getItem("products"))
 }
+
+// Renderizado de productos
+let productContainer = document.getElementById("product-list")
+function renderProducts(products) {
+	products.forEach(product => {
+		const card = document.createElement("div")
+		card.innerHTML = `<h3>${product.title}</h3>
+							<p>Descripción: ${product.description}</p>
+							<p>Precio: $${product.price}</p>
+							<p>Stock: ${product.stock} unidades</p>
+							<button class="buyProduct1" id="${product.id}">Comprar (1)</button>
+							<button class="buyProduct5" id="${product.id}">Comprar (5)</button>
+							<button class="buyProduct10" id="${product.id}">Comprar (10)</button>`
+		productContainer.appendChild(card)
+	});
+}
+renderProducts(products)
+
+// Inicialización de fondos
+let fundsFront = document.getElementById("funds-amount");
+let funds = 0
+if (localStorage.getItem("funds") == null) {
+	localStorage.setItem("funds", funds)
+} else {
+	funds = Number(localStorage.getItem("funds"))
+	fundsFront.innerHTML = funds;
+}
+
+// Renderizado de fondos
+let addFunds10Button = document.getElementById("add-funds10")
+let addFunds100Button = document.getElementById("add-funds100")
+addFunds10Button.onclick = () => {
+	funds = Number(localStorage.getItem("funds"))
+	funds+=10;
+	localStorage.setItem("funds", funds)
+	fundsFront.innerHTML = funds;
+}
+addFunds100Button.onclick = () => {
+	funds = Number(localStorage.getItem("funds"))
+	funds+=100;
+	localStorage.setItem("funds", funds)
+	fundsFront.innerHTML = funds;
+}
+
+let historyButton = document.getElementById("history")
+
+// Inicialización de historial de compra
+let purchaseHistory = []
+if (localStorage.getItem("purchaseHistory") == null) {
+	localStorage.setItem("purchaseHistory", JSON.stringify(purchaseHistory))
+} else {
+	purchaseHistory = JSON.parse(localStorage.getItem("purchaseHistory"))
+}
+
+// Función de agregar una compra
+function addToPurchaseHistory(className,quantity) {
+	buyButtom = document.querySelectorAll(className)
+	buyButtom.forEach(buttom => {
+		buttom.onclick = (e) => {
+			const productId = e.currentTarget.id
+			console.log(productId,className,quantity)
+			const foundProduct = products.find(product => product.id == productId)
+			if (funds>=foundProduct.price*quantity && foundProduct.stock>=quantity){
+				funds -= foundProduct.price*quantity
+				localStorage.setItem("funds", funds)
+				fundsFront.innerHTML = funds
+				let productIndex = products.indexOf(foundProduct)
+				foundProduct.stock -= quantity
+				products[productIndex] = foundProduct
+				localStorage.setItem("products", JSON.stringify(products))
+				products.splice()
+				const purchase = {
+					"date": Date(),
+					"quantity": quantity,
+					"product": foundProduct
+				}
+				purchaseHistory.push(purchase)
+				localStorage.setItem("purchaseHistory", JSON.stringify(purchaseHistory))
+				e.currentTarget.innerHTML = "Comprado"
+			} else {
+				e.currentTarget.innerHTML = "Error"
+			}
+		}
+	});
+}
+addToPurchaseHistory(".buyProduct1",1)
+addToPurchaseHistory(".buyProduct5",5)
+addToPurchaseHistory(".buyProduct10",10)
